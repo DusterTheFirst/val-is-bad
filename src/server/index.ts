a56fe01cb7ece1c742cb7ca4bfc943b06ee0711b
Maksim Sinik <maksim@sinik.it>
2017-11-03 19:33:43 +0100

    adds base files and dependencies

---
+++ src/server/index.ts
@@ -0,0 +1,28 @@
+import * as next from 'next'
+import * as fastify from 'fastify'
+import { join } from 'path'
+
+import routes from './api/index'
+
+const port = parseInt(process.env.PORT as string) || 3000
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev, dir: join(__dirname, '../client') })
+const handle = app.getRequestHandler()
+
+app.prepare()
+  .then(() => {
+    const server = fastify()
+
+    server.use('/api', routes)
+
+    server.get('/*', (req, res: any) => {
+      handle(req.req, res.res)
+    })
+
+    server.listen(port, (err) => {
+      if (err) {
+        throw err
+      }
+      console.log(`> Ready on http://localhost:${port}`)
+    })
+  })
